9fa1539d7b9184e92973a37c183aed9afb266c2b
Ravi <ravikumr0403@gmail.com>
2022-04-26 10:22:43 +0530

    feat: Edit name

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -2,13 +2,26 @@ import { useUser } from "../context";
 import { DigitalClock } from "./DigitalClock";
 import { MainFocus } from "./MainFocus";
 import { GoogleSearch } from "./GoogleSearch";
+import { FiEdit3 } from "react-icons/fi";
 
 export const MainContent = () => {
-  const { user } = useUser();
+  const { setOptimUser, user } = useUser();
+  const editName = () => {
+    localStorage.removeItem("user");
+    setOptimUser("");
+  };
   return (
     <>
       <div className="h-full flex items-center justify-center flex-col">
-        <h1 className="text-4xl">Hey {user}</h1>
+        <div className="group">
+          <h1 className="text-4xl inline-block">Hey {user} </h1>
+          <button
+            className="text-slate-200 hidden group-hover:inline-block mx-3"
+            onClick={editName}
+          >
+            <FiEdit3 />
+          </button>
+        </div>
         <DigitalClock />
         <GoogleSearch />
         <MainFocus />
